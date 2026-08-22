726b2aa03dcd567071ed58e63c880ef49959087e
saurabh shukla <saurabhkumarshukla4075@gmail.com>
2026-08-22 17:38:36 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/-GlobeTrotter-/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
